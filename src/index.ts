293c2a4715d4c4e467073fb6456eaceae71e037f
Lior Steinberg <liorsht@wix.com>
2026-04-23 15:35:01 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("Hello World!");
+    const body = await request.json<{ name: string }>();
+    return new Response(JSON.stringify({ message: `Hello ${body.name}` }), {
+      headers: { "Content-Type": "application/json" },
+    });
   },
 } satisfies ExportedHandler<Env>;
